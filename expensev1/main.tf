585f817087cc36fc5e2585fb21918399168db2ff
kp3073 <keyurpatel8288@gmail.com>
2024-06-21 14:14:22 -0400

    1

--- expensev1/main.tf
+++ expensev1/main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "frontend" {
   ami = data.aws_ami.ami.id
   instance_type = "t3.micro"
   vpc_security_group_ids = [data.aws_security_group.sg.id]
-
+  tags = frontend
 
 }
 
@@ -11,7 +11,7 @@ resource "aws_instance" "backend" {
   ami = data.aws_ami.ami.id
   instance_type = "t3.micro"
   vpc_security_group_ids = [data.aws_security_group.sg.id]
-
+  tags = backend
 
 }
 
@@ -20,4 +20,5 @@ resource "aws_instance" "mysql" {
   ami = data.aws_ami.ami.id
   instance_type = "t3.micro"
   vpc_security_group_ids = [data.aws_security_group.sg.id]
+  tags = mysql
 }
