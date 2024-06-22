992439ef1232b8632ff7bebe20409365623a7eed
kp3073 <keyurpatel8288@gmail.com>
2024-06-22 00:27:10 -0400

    1

--- expensev1/main.tf
+++ expensev1/main.tf
@@ -47,7 +47,7 @@ resource "aws_instance" "backend" {
   instance_type = "t3.micro"
   vpc_security_group_ids = [data.aws_security_group.sg.id]
   tags = {
-    name = "mysql"
+    name = "backend"
   }
 }
 
