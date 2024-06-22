27973379369aa2b7330e2b30c1d4ee13138073af
kp3073 <keyurpatel8288@gmail.com>
2024-06-22 00:16:53 -0400

    1

--- expensev1/main.tf
+++ expensev1/main.tf
@@ -20,21 +20,45 @@ resource "null_resource" "frontend" {
 }
 
 
-# resource "aws_instance" "backend" {
-#   ami = data.aws_ami.ami.id
-#   instance_type = "t3.micro"
-#   vpc_security_group_ids = [data.aws_security_group.sg.id]
-#   tags = {
-#     name = "backend"
-#   }
-# }
+resource "aws_instance" "mysql" {
+  ami = data.aws_ami.ami.id
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [data.aws_security_group.sg.id]
+  tags = {
+    name = "mysql"
+  }
+}
+
+resource "null_resource" "mysql" {
+  depends_on = [ aws_instance.mysql ]
+  provisioner "local-exec" {
+    command = <<EOF
+    cd /root/Expense-ansible
+    git pull
+    sleep 60
+    ansible-playbook -i ${aws_instance.mysql.private_ip}, -e ansible_username=centos -e ansible_password=DevOps321 main.yml -e role_name=mysql
+    EOF
+  }
+}
 
 
-# resource "aws_instance" "mysql" {
-#   ami = data.aws_ami.ami.id
-#   instance_type = "t3.micro"
-#   vpc_security_group_ids = [data.aws_security_group.sg.id]
-#   tags = {
-#     name = "mysql"
-#   }
-# }
+resource "aws_instance" "backend" {
+  ami = data.aws_ami.ami.id
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [data.aws_security_group.sg.id]
+  tags = {
+    name = "mysql"
+  }
+}
+
+resource "null_resource" "backend" {
+  depends_on = [ aws_instance.backend ]
+  provisioner "local-exec" {
+    command = <<EOF
+    cd /root/Expense-ansible
+    git pull
+    sleep 60
+    ansible-playbook -i ${aws_instance.backend.private_ip}, -e ansible_username=centos -e ansible_password=DevOps321 main.yml -e role_name=backend
+    EOF
+  }
+}
