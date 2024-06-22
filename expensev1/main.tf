78ba1e0f40f8bb553093fe33ce91ebf23366f7a3
kp3073 <keyurpatel8288@gmail.com>
2024-06-22 00:02:18 -0400

    1

--- expensev1/main.tf
+++ expensev1/main.tf
@@ -5,13 +5,15 @@ resource "aws_instance" "frontend" {
   tags =  {
     name = "frontend"
   }
+}
 
+resource "null_resource" "frontend" {
   provisioner "local-exec" {
     command = <<EOF
     cd /root/Expense-ansible
     git pull
     sleep 60
-    ansible-playbook -i ${self.private_ip}, -e ansible_username=centos -e ansible_password=DevOps321 main.yml -e role_name=frontend
+    ansible-playbook -i ${aws.instance.frontend.private_ip}, -e ansible_username=centos -e ansible_password=DevOps321 main.yml -e role_name=frontend
     EOF
   }
 }
