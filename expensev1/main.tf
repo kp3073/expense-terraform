762cdb377a00830d77a4667e7e3276de52865f94
kp3073 <keyurpatel8288@gmail.com>
2024-06-21 23:50:03 -0400

    1

--- expensev1/main.tf
+++ expensev1/main.tf
@@ -5,24 +5,33 @@ resource "aws_instance" "frontend" {
   tags =  {
     name = "frontend"
   }
-}
 
-
-resource "aws_instance" "backend" {
-  ami = data.aws_ami.ami.id
-  instance_type = "t3.micro"
-  vpc_security_group_ids = [data.aws_security_group.sg.id]
-  tags = {
-    name = "backend"
+  provisioner "local-exec" {
+    command = <<EOF
+    cd /root/Expense-ansible
+    git pull
+    sleep 60
+    ansible-playbook -i ${self.private_ip}, -e ansible_username=centos -e ansible_password=DevOps321 expense.yml -e role_name=frontend
+    EOF
   }
 }
 
 
-resource "aws_instance" "mysql" {
-  ami = data.aws_ami.ami.id
-  instance_type = "t3.micro"
-  vpc_security_group_ids = [data.aws_security_group.sg.id]
-  tags = {
-    name = "mysql"
-  }
-}
+# resource "aws_instance" "backend" {
+#   ami = data.aws_ami.ami.id
+#   instance_type = "t3.micro"
+#   vpc_security_group_ids = [data.aws_security_group.sg.id]
+#   tags = {
+#     name = "backend"
+#   }
+# }
+
+
+# resource "aws_instance" "mysql" {
+#   ami = data.aws_ami.ami.id
+#   instance_type = "t3.micro"
+#   vpc_security_group_ids = [data.aws_security_group.sg.id]
+#   tags = {
+#     name = "mysql"
+#   }
+# }
